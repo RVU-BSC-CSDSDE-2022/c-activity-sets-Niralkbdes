1de8f5be5166fe65ae5665057006ac5d676311eb
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-11-30 04:27:33 +0000

    got it

--- .ccls-cache/@home@runner@c-activity-sets-Niralkbdes-57/set04@problem01.c
+++ .ccls-cache/@home@runner@c-activity-sets-Niralkbdes-57/set04@problem01.c
@@ -1,5 +1,5 @@
 # include <stdio.h>
-# include <math.h>
+
 void input(float *x1, float *y1, float *x2, float *y2);
 float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
