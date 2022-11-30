67f1037e33abf657c094eda5f1b81a01d68e0226
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-11-30 05:14:46 +0000

    Done it

--- set04/problem01.c
+++ set04/problem01.c
@@ -33,5 +33,5 @@ void input(float *x1, float *y1, float *x2, float *y2)
 
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-  printf("The distance between the two points(%f,%f) and (%f,%f) is %f" , x1,x2,y1,y2, distance);
+  printf("The distance between the two points (%f,%f) and (%f,%f) is %f" , x1,x2,y1,y2, distance);
 }
