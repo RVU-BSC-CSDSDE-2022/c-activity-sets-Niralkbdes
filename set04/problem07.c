5556200fe0aa57e581efbc17f4c9e6c98ae4aa6f
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-11-30 10:59:03 +0000

    got it

--- set04/problem07.c
+++ set04/problem07.c
@@ -2,17 +2,19 @@
 #include<math.h>
 typedef struct point
 {
-    float x, y;
-} Point;
-
+  float x, y;
+} 
+Point;
 typedef struct line 
 {
     Point p1, p2;
     float distance;
-} Line;
+} 
+Line;
 
 Point input_point()
-{ Point p;
+{ 
+  Point p;
   printf("Enter the x cordinate:\n");
   scanf("%f",&p.x);
 
@@ -22,14 +24,13 @@ Point input_point()
 }
 Line input_line()
 { Line l;
-  Point p1,p2;
   l.p1= input_point();
   l.p2= input_point();
   return l;
 }
 void find_length(Line *l)
 {
-  l->distance= sqrt(pow((l->p2.x-l->p1.x),2)+pow((l->p2.y-l->p1.y),2));
+  l->distance = sqrt(pow((l-> p2.x - l-> p1.x ),2)+pow((l-> p2.y - l-> p1.y ),2));
 }
 void output(Line l)
 {
