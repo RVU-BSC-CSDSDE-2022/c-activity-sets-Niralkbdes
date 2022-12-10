3369350919a6e61cfef3ec5e76be6dc89617da52
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-12-10 14:07:50 +0000

    Done again

--- set02/problem01.c
+++ set02/problem01.c
@@ -9,39 +9,37 @@ typedef struct _point Point;
 
 Point input()
 {
- Point a;
-printf("Enter the x-coordinates of the point");
-scanf("%f", &a.x);
-printf("Enter the y-coordinates of the point");
-scanf("%f", &a.y);
-return a;
+  Point a;
+  printf("Enter the x co-ordinates of the point");
+  scanf("%f",&a.x);
+  printf("Enter the y co-ordinates of the point");
+  scanf("%f",&a.y);
+  return a;
 }
 
 float dist(Point e, Point f)
 {
   float res1,res2;
-  float distance , p,q,r;
-  res1 = f.x - e.x;
-  res2 = f.y - e.y;
-  p = pow(res1 , 2);
-  q = pow(res2 , 2);
-  r = p+q;
+  float distance,p,q,r;
+  res1 = f.x-e.x;
+  res2 = f.y-e.y;
+  p=pow(res1,2);
+  q=pow(res2,2);
+  r=p+q;
   distance = sqrt(r);
+  
   return distance;
-  }
-
+}
 void output(Point a, Point b, float distance)
 {
-  printf("The distance between the point %f %f and point %f %f is %f", a.x, a.y, b.x, b.y, distance);
-  }
-
+  printf("The distance between Point %f,%f and Point %f ,%f is %f",a.x,a.y,b.x,b.y,distance);
+}
 int main()
 {
   Point e,f;
   float g;
-  e = input ();
-  f = input ();
-  g= dist(e,f);
-  output (e,f,g);
-  return 0;
+  e= input();
+  f= input();
+  g=dist(e,f);
+ output(e,f,g);
 }
--- set02/problem02.c
+++ set02/problem02.c
@@ -1,32 +1,34 @@
-#include<stdio.h>
-#include<math.h>
-void input_camel_details(float *radius,float *height,float *length)
+#include <stdio.h>
+#include <math.h>
+
+void input_camel_details(float *radius, float *height, float *length)
 {
-  printf("Enter the radius\n");
-  scanf("%f",radius);
+  printf("Enter the radius");
+scanf("%f", radius);
 
-  printf("Enter the height\n");
-  scanf("%f",height);
+printf("Enter the height");
+scanf("%f", height);
 
-  printf("Enter the lenght\n");
-  scanf("%f",length);
+printf("Enter the length");
+scanf("%f", length);
   }
-  float find_weight(float radius,float height,float lenght)
-  { 
-    float weight;
-    int a,b,c;
-    a= pow(radius,3);
-    b= height*lenght;
-    c= sqrt(b);
-    weight= 3.14*a*c;
-    return weight;
-    
+
+float find_weight(float radius, float height, float length)
+{
+float weight;
+int a,b,c;
+a = pow(radius, 3);
+b = height*length;
+c = sqrt(b);
+weight = 3.14*a*c;
+return weight;
   }
-  int main()
-  {
-    float e,f,g,h;
-    input_camel_details(&e,&f,&g);
-    h = find_weight(e,f,g);
-    printf("The weight of camel is %f\n",h);
-    return 0;
-  }
+
+int main()
+{
+  int e,f,g,h;
+  input_camel_details( &e, &f ,&g);
+  h = find_weight(e,f,g);
+  printf("The weight of the camel is %f\n" ,h);
+  return 0;
+}
