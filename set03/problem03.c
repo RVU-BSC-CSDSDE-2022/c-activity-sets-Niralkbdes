023b8f78f9a9d24a89c7b0d818462706b019647e
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-12-13 10:08:14 +0000

    Learnt and done without seeing

--- set03/problem03.c
+++ set03/problem03.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 int input_number();
 int is_composite(int n);
@@ -5,36 +6,35 @@ void output(int n, int result);
 
 int input_number()
 {
-  int n;
-  printf("Enter a number\n");
-  scanf("%d",&n);
-  return n;
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    return n;
 }
 
 int is_composite(int n)
 {
-  for (int i=3;i<n;i++)
+    for (int i=3; i<n; i++)
     {
-      if(n %i == 0)
+        if (n%i==0)
         return 1;
     }
     return 0;
 }
-void output(int n, int is_composite)
+
+void output(int n, int result)
 {
-  if(is_composite == 0)
-  {
-    printf("%d is not a composite number\n");
-  }
-  else {
-    printf("%d is a composite number\n");
-  }
+    if (is_composite==0)
+    printf("The number %d is not composite\n", n);
+    else
+     printf("The number %d is composite\n", n);
 }
+
 int main()
 {
-  int n,isc;
-  n = input_number();
-  isc = is_composite(n);
-  output(n,isc);
-  return 0;
+    int n,isc;
+    n = input_number();
+    isc = is_composite(n);
+    output(n,isc);
+    return 0;
 }
