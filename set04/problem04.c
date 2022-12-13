58aafb5e9087a3ac06e03b7ae6706450bffc2d4b
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-12-13 05:11:31 +0000

    Learnt and written again without seeing

--- set04/problem04.c
+++ set04/problem04.c
@@ -4,21 +4,19 @@ int input();
 int find_fibo(int n);
 void output(int n, int fibo);
 
-
 int main()
 {
-  int n,fibo;
-  n =input(n);
+  int n, fibo;
+  n = input (n);
   fibo = find_fibo(n);
-  output(n,fibo);
+  output (n, fibo);
   return 0;
-}
-
+  }
 
 int input()
 {
   int n;
-  printf("Enter the number\n");
+  printf("Enter any number\n");
   scanf("%d", &n);
   return n;
 }
@@ -27,14 +25,14 @@ int find_fibo(int n)
 {
   int a[n+1];
   a[0]=0, a[1]=1;
-  for ( int i=2; i<=n; i++)
+  for (int i=2;i<=n;i++)
     {
-      a[i]= a[i-1]+a[i-2];
+        a[i]= a[n-1]+a[n-2];
     }
   return a[n];
 }
 
 void output(int n, int fibo)
 {
-  printf("fibo(%d) is %d" ,n,fibo);
+  printf("fibo(%d) is %d", n, fibo);
 }
