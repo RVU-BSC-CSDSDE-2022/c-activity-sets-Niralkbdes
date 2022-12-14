31326a046184bd54b9cdd5e0172ec47dc33948b6
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-12-14 15:59:32 +0000

    Learnt and done again

--- set04/problem06.c
+++ set04/problem06.c
@@ -29,14 +29,13 @@ int sub_str_index(char* string, char* substring)
   int len1,len2, index=-1, count=0;
   len1= strlen(string);
   len2= strlen(substring);
-  for (int i=0; string[i]!= '\0' && substring[i]!='\0' ; i++)
+  for (int i=0; string[i]!= '\0' ; i++)
     {
       if(string[i]==substring[0])
         {
         index=i;
         for(int j=0,k=i;
-          substring[j]!='\0' &&
-          string[k]!= '\0' ; j++,k++)
+          substring[j]!='\0'; j++,k++)
           {
             if(string[k]==substring [j])
               count++;
