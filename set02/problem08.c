4a86e639602e9e92c8567ba79c5748d6f28ad211
Niral  Koul <113001953+Niralkbdes@users.noreply.github.com>
2022-12-19 19:33:33 +0000

    Got it

--- set02/problem08.c
+++ set02/problem08.c
@@ -1,5 +1,5 @@
-#inculde <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 typedef struct _camel 
 {
     float radius, height, length, weight;
@@ -8,7 +8,7 @@ Camel;
 int input_n()
 {
   int n;
-  printf("Enter the number of camels");
+  printf("Enter the number of camels:");
   scanf("%d",&n);
   return n;
 }
@@ -27,30 +27,28 @@ Camel input_camel()
 void input(int n, Camel c[n], float *truck_weight)
 {
   for(int i=0;i<n;i++)
-    c[i] = input_camel()
+  {c[i] = input_camel();}
   printf("Enter truck weight\n");
   scanf("%f",truck_weight);
 }
-
 void find_weight(Camel *c) 
 {
-   c->weight= 3.14*( pow(c->radius,3))*(sqrt(c->height*c->length));
-   
+   c->weight= 3.14*(pow(c->radius,3))*(sqrt(c->height*c->length));
 }
 float find_camel_weight(int n, Camel c[n])
 {
   float total_weight=0;
-  for(int i = 0; i<n; i++)
+  for(int i = 0; i<n; i++) 
   {
     find_weight(&c[i]);
     total_weight += c[i].weight;
   }
   return total_weight;
 }
-float compute_total_weight(int n, Camel c[n], float truck_weight)
+ float compute_total_weight(int n, Camel c[n], float truck_weight)
 {
  float total_weight = find_camel_weight(n,c);
- total_weight += truck_weight;
+ total_weight+=truck_weight;
  return total_weight;
 }
 void output(float total_weight)
@@ -58,12 +56,11 @@ void output(float total_weight)
   printf("Total weight of the truck with camels = %f",total_weight);
 }
 int main()
-{
-  int num = input_n();
+{ float truck_weight, total_weight;
+  int n = input_n();
   Camel c[n];
-  float truck_weight, total_weight;
-  input(num,c,&truck_weight);
-  total_weight = compute_total_weight(num,c,truck_weight);
+  input(n,c,&truck_weight);
+  total_weight = compute_total_weight(n,c,truck_weight);
   output(total_weight);
   return 0;
 }
